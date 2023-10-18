d89baa437ca35a07a0db4b24713692600eac4b72
yamaryosan <canda3267@gmail.com>
2023-10-18 18:58:15 +0900

    [add] App追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.title = props.title;
+    this.message = props.message;
+  }
+  
+  render(){
+    return <div>
+    <h1 className="bg-primary text-white display-4">React</h1>
+    <div className="container">
+      <p className="subtitle">{this.title}</p>
+      <p>{this.message}</p>
     </div>
-  );
+  </div>
+  }
 }
 
 export default App;
