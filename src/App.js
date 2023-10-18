a068f02606b4c6178a293560bba2462085864533
yamaryosan <canda3267@gmail.com>
2023-10-18 20:51:03 +0900

    [update] ステートの使用

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Rect from './Rect';
 class App extends Component {
   constructor(props){
     super(props);
-    this.title = props.title;
-    this.message = props.message;
+    this.state = {
+      msg:'Hello'
+    }
   }
   
   render(){
     return <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
-        <p className="subtitle">draw rectangle.</p>
-        <Rect x="200" y="200" width="200" height="200" color="#6ff9" radius="25"/>
-        <Rect x="300" y="300" width="200" height="200" color="#f6f9" radius="75"/>
-        <Rect x="400" y="400" width="200" height="200" color="#6669" radius="100"/>
+        <p className="subtitle">Show message.</p>
+        <p className="alert alert-warning">{this.state.msg}</p>
+        <p className="alert alert-dark">{this.state.msg}</p>
       </div>
     </div>
   }
