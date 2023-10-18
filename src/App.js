6d2e9090a374d4b01f64bed0f103d8ef284c1def
yamaryosan <canda3267@gmail.com>
2023-10-18 21:57:51 +0900

    [update] タイマーセット

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      msg:'Hello'
+      msg:'Hello',
+      count: 0,
     }
+
+    let timer = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+        msg: 'count: ' + this.state.count,
+      })
+    }, 1000)
   }
   
   render(){
     return <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
-        <p className="subtitle">Show message.</p>
+        <p className="subtitle">Count number.</p>
         <p className="alert alert-warning">{this.state.msg}</p>
-        <p className="alert alert-dark">{this.state.msg}</p>
+        <p className="alert alert-dark">{this.props.msg}</p>
       </div>
     </div>
   }
