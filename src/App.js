04c293d6331bce6d74d1069942d09325488ba728
yamaryosan <canda3267@gmail.com>
2023-10-18 23:24:44 +0900

    [update] ステート値によって表示を変更

--- src/App.js
+++ src/App.js
@@ -4,27 +4,41 @@ import Rect from './Rect';
 
 class App extends Component {
   constructor(props){
-    super(props);
+    super(props)
     this.state = {
-      msg:'Hello',
-      count: 0,
+      counter: 0,
+      msg: "count start",
+      flag: true,
     }
+    this.doAction = this.doAction.bind(this)
+  }
 
-    let timer = setInterval(() => {
-      this.setState({
-        count: this.state.count + 1,
-        msg: 'count: ' + this.state.count,
-      })
-    }, 1000)
+  doAction(event) {
+    this.setState({
+      counter: this.state.counter + 1,
+      msg: this.state.counter,
+      flag: !this.state.flag
+    })
   }
   
   render(){
     return <div>
-      <h1 className="bg-primary text-white display-4 ">React</h1>
+      <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <p className="subtitle">Count number.</p>
-        <p className="alert alert-warning">{this.state.msg}</p>
-        <p className="alert alert-dark">{this.props.msg}</p>
+        {
+          this.state.flag ?
+          <div className="alert alert-warning text-right">
+            <p className="h5">count: {this.state.msg}</p>
+          </div>
+          :
+          <div className="alert alert-warning text-left">
+            <p className="h5">{this.state.msg}</p>
+          </div>
+        }
+        <div className="text-center">
+          <button className="btn btn-primary" onClick={this.doAction}>Click</button>
+        </div>
       </div>
     </div>
   }
