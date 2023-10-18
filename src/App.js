e3ac7e8226714a525ac69152412f6b2ffe9418bc
yamaryosan <canda3267@gmail.com>
2023-10-18 20:44:31 +0900

    [update] 透明度も反映

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ class App extends Component {
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <p className="subtitle">draw rectangle.</p>
-        <Rect x="200" y="200" width="200" height="200" color="#66ff99" radius="25"/>
-        <Rect x="300" y="300" width="200" height="200" color="#ff6f99" radius="75"/>
-        <Rect x="400" y="400" width="200" height="200" color="#666699" radius="100"/>
+        <Rect x="200" y="200" width="200" height="200" color="#6ff9" radius="25"/>
+        <Rect x="300" y="300" width="200" height="200" color="#f6f9" radius="75"/>
+        <Rect x="400" y="400" width="200" height="200" color="#6669" radius="100"/>
       </div>
     </div>
   }
