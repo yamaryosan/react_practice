5ec359937d736a7b870fa977726fb3acfaa8903e
yamaryosan <canda3267@gmail.com>
2023-10-22 22:51:47 +0900

    [update] 関数コンポーネントでフォームを実装

--- src/App.js
+++ src/App.js
@@ -2,19 +2,76 @@ import React, { useState } from 'react';
 import './App.css';
 // import Rect from './Rect';
 
+function AlertMessage(props) {
+  const data = props.data
+  const msg = JSON.stringify(data)
+
+  return <div className="alert alert-primary h5 text-primary">
+    <h5>{msg}</h5>
+    <hr />
+    <table className="table h6">
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <td>{data.name}</td>
+        </tr>
+        <tr>
+          <th>Mail</th>
+          <td>{data.mail}</td>
+        </tr>
+        <tr>
+          <th>Age</th>
+          <td>{data.age}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+}
+
 function App() {
-  const [message] = useState('Welcome to React!')
+  const [name, setName] = useState('')
+  const [mail, setMail] = useState('')
+  const [age, setAge] = useState(0)
+  const [form, setForm] = useState({
+    name: 'no name', mail: 'no mail', age: 0
+  })
+
+  const doChangeName = (event) => {
+    setName(event.target.value)
+  }
+  const doChangeMail = (event) => {
+    setMail(event.target.value)
+  }
+  const doChangeAge = (event) => {
+    setAge(event.target.value)
+  }
+
+  const doSubmit = (event) => {
+    setForm({ name: name, mail: mail, age: age })
+    event.preventDefault()
+  }
 
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <h4 classname="my-3">
-          Hooks sample
-        </h4>
-        <div className="alert alert-primary text-center">
-          <p className="h5">{message}</p>
-        </div>
+        <h4 className="my-3">Hooks Sample</h4>
+        <AlertMessage data={form} setData={setForm} />
+        <form onSubmit={doSubmit}>
+          <div className="form-group">
+            <label>Name:</label>
+            <input type="text" className="form-control" onChange={doChangeName} />
+          </div>
+          <div className="form-group">
+            <label>Mail:</label>
+            <input type="text" className="form-control" onChange={doChangeMail} />
+          </div>
+          <div className="form-group">
+            <label>Age:</label>
+            <input type="number" className="form-control" onChange={doChangeAge} />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Click" />
+        </form>
       </div>
     </div>
   )
